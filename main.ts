ce725e4c968d8fe97fe66377eb46dcb7c161ab2d
Dgarc359 <dgarc359+github-personal@gmail.com>
2024-01-17 01:17:11 -0500

    add starter code

---
+++ main.ts
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+import { program } from "commander";
+import dotenv from "dotenv";
+
+dotenv.config();
+// todo: get env vars for things like pointing template blocks to specific file urls
+
+program
+  .command("generate")
+  .option("-cg, --contribution-guide <bool>")
+  .action(() => {
+    console.log("hello world");
+  });
+
+program.parse();
